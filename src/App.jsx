a12131566045c208e727e56f9c198190f2c93ddc
Gustavo Lastra <lastraemiliano1@gmail.com>
2025-01-23 13:32:23 -0600

    perfil e informacion

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,35 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline ">
-    Hello world!
-  </h1>
+      <div>
+        <div>
+          <img className='rounded-t-2xl border-7 border-neutral-400 w-30 h-30' src="https://randomuser.me/api/portraits/men/51.jpg" alt="perfil" />
+        </div>
+        <div>
+          <span>
+            Followers
+          </span>
+          <span>
+            27839
+          </span>
+        </div>
+        <div>
+          <span>
+            Following
+          </span>
+          <span>
+            0
+          </span>
+        </div>
+        <div>
+          <span>
+            Location
+          </span>
+          <span>
+            San Francisco, CA
+          </span>
+        </div>
+      </div>
     </>
   )
 }
