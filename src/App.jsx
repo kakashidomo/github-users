d3b03a7996bbcc14fd0aca8a0854f72de54dfb27
Guillermo Sangerman <sangermanmptn8@gmail.com>
2025-01-23 13:28:08 -0600

    se agrego los complementos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Container } from './components/Container'
 
 
 function App() {
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline ">
-    Hello world!
-  </h1>
+      <Container/>
     </>
   )
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Container() {
+  return (
+    
+    <>
+        <div className='w-full h-screen'>
+            <div className='h-1/3 bg-black '>
+
+            </div>
+            <div className='h-2/3 bg-blue-600 '>
+
+            </div>
+        </div>
+    </>
+  )
+}
