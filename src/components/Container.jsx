e0618e96795391a25e1ab1f23a1bf7b207e076c5
Guillermo Sangerman <sangermanmptn8@gmail.com>
2025-01-23 15:02:23 -0600

    primeros cambios

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,10 +5,14 @@ export function Container() {
     
     <>
         <div className='w-full h-screen'>
-            <div className='h-1/3 bg-black '>
-
+            <div className=' h-1/5 bg-black text-[#97A3B6]'>
+                <div className='flex fixed mx-50 mt-8 bg-[#20293A] rounded-xl'>
+                <label htmlFor='search'  className=''><img src="./desing/Search.svg" alt="" /></label>
+                <input id='search' className='bg-[#20293A] ' placeholder='UserName' type="text" />
+                </div>
+                <img className='h-[100%] object-fill' src="./desing/hero-image-github-profile-sm.jpg" alt="" />
             </div>
-            <div className='h-2/3 bg-blue-600 '>
+            <div className='h-4/5 bg-[#20293A] '>
 
             </div>
         </div>
